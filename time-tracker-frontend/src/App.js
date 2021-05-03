52816949828ed14d8e6e0188bd2dfde05f786209
Sampath Kumar <sampath.kumar@mail.waldenu.edu>
2021-05-03 21:57:20 +0530

    Initial commit

---
+++ time-tracker-frontend/src/App.js
@@ -0,0 +1,12 @@
+import TaskAddForm from './components/task-add-form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskAddForm/>
+    </div>
+  );
+}
+
+export default App;
