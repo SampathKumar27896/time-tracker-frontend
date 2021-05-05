b779a9203b0a58f35a176b5ab63a9ac093b93392
Sampath Kumar <sampath.kumar@mail.waldenu.edu>
2021-05-06 00:15:42 +0530

    worked on displaying the task list - Displayed the task names

--- time-tracker-frontend/src/App.js
+++ time-tracker-frontend/src/App.js
@@ -1,10 +1,12 @@
 import TaskAddForm from './components/task-add-form';
+import TaskList from './components/task-list';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TaskAddForm/>
+      <TaskList/>
     </div>
   );
 }
---
+++ time-tracker-frontend/src/components/task-list.js
@@ -0,0 +1,49 @@
+import {useEffect, useState} from 'react';
+import '../App.css';
+
+function TaskList() {
+    const [taskList, updateTaskList] = useState([]);
+
+    useEffect(() => {
+        
+            fetch('http://localhost:3001/list_tasks',{
+                method: "GET",
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "text/plain"
+                }})
+              .then(results => results.json())
+              .then(data => {
+                updateTaskList([...data.data]);
+              });
+    },[])
+
+  return (
+    <div className="task-list">
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Task name</th>
+            <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+           {
+               taskList.map(task=> (
+                (<tr>
+                    <th scope="row">{task.id}</th>
+                    <td>{task.task_name}</td>
+                    <td>{task.start_time}</td>
+                    <td>{task.end_time}</td>
+                    </tr>
+                )
+               ))
+           }
+        </tbody>
+        </table>
+    </div>
+  );
+}
+
+export default TaskList;
