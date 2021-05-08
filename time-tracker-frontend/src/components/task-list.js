0657fa86b1e9d8b0a17155fe90ef6b209db86cd1
Sampath Kumar <sampath.kumar@mail.waldenu.edu>
2021-05-08 22:51:19 +0530

    added a basic timer function for the project

--- time-tracker-frontend/src/components/task-list.js
+++ time-tracker-frontend/src/components/task-list.js
@@ -4,7 +4,7 @@ import '../App.css';
 
 function TaskList() {
     const [taskList, updateTaskList] = useState([]);
-
+    const [play, updatePlay] = useState(false)
     useEffect(() => {
         
             fetch('http://localhost:3001/list_tasks',{
@@ -18,7 +18,36 @@ function TaskList() {
                 updateTaskList([...data.data]);
               });
     },[])
+    const handlePlayPause = (e) => {
+      updatePlay(!play);
+    }
+    const startTimer = () => {
+      var countDownDate = new Date().getTime();
+      var timer = setInterval(function() {
 
+        // Get today's date and time
+        var now = new Date().getTime();
+          
+        // Find the distance between now and the count down date
+        var distance = now - countDownDate;
+          
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+          
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+          
+        // If the count down is over, write some text 
+        if (distance < 0) {
+          clearInterval(startTimer);
+          document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+    }
   return (
     <div className="task-list">
         <table class="table">
@@ -37,7 +66,7 @@ function TaskList() {
                     <th scope="row">{task.id}</th>
                     <td>{task.task_name}</td>
                     <td>{task.duration}</td>
-                    <td><TaskAction/></td>
+                    <td><TaskAction handlePlayPause={handlePlayPause}/></td>
                     </tr>
                 )
                ))
